e022ece1d3604e80b73cd845a4c255bbee4e6950
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-10 14:23:23 +0100

    ex04 Storing values in variables

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,9 +1,9 @@
 function main() {
     //Only change code below this line
     var x = 10;
-    var y = var x;
+    var y = x;
     //Only change code above this line
-    return[x,y];
+    return [x,y];
 }
 console.log(main());
 module.exports = main;
