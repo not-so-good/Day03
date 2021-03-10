84584e3b368b30061323e9557120b4d787d5d2dd
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-10 12:01:25 +0100

    ex02 learning how to comment

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // This is me commentig in a single line
+    /* This is me commentig in a multi-line
+    This is me commentig in a multi-line
+    This is me commentig in a multi-line
+    This is me commentig in a multi-line
+    This is me commentig in a multi-line*/
+
+}
+main();
+module.exports = main;
